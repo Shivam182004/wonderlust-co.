aa32c585582aad8aec20c669bfef4163da1377d5
RishiTiwari-18 <153971668+RishiTiwari-18@users.noreply.github.com>
2023-12-26 10:39:13 +0000

    added menu ,cursor, fixed navbar, shery js

---
+++ script.js
@@ -0,0 +1,82 @@
+Shery.makeMagnet(".menu-button" , {
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+
+
+function mymenu(){
+    let menu = document.querySelector(".menu-button")
+let menuclose = document.querySelector(".menu-close")
+
+menu.addEventListener("click", function(){
+    gsap.to(".menu",{
+        opacity:1,
+        width:"50%",
+    })
+
+    // gsap.to("#main",{
+    //     opacity:.5,
+    // })
+})
+
+menuclose.addEventListener("click",function(){
+    gsap.to(".menu",{
+        opacity:0,
+        width:"0%"
+       
+    })
+
+    // gsap.to("#main",{
+    //     opacity:1,
+    // })
+})
+}
+
+mymenu()
+
+function mycursor(){
+    let cursor = document.querySelector(".cursor")
+    let main = document.querySelector("#main")
+    window.addEventListener("mousemove",function(dets){
+        gsap.to(cursor,{
+            x: dets.x-5,
+            y: dets.y-5,
+            duration:1,
+            ease: "power4.out",
+        })
+    })
+
+    main.addEventListener("mouseenter",function(dets){
+        gsap.to(cursor,{
+           scale:1,
+            ease: "power4.out",
+        })
+    })
+
+    main.addEventListener("mouseleave",function(dets){
+        gsap.to(cursor,{
+            scale:0,
+            ease: "power4.out",
+        })
+    })
+    
+    window.addEventListener("dblclick",function(dets){
+       let tl = gsap.timeline()
+    
+        tl.to(cursor,{
+        scale: 2,
+        duration:.3,
+        ease: "power4.out",
+        });
+    
+        tl.to(cursor,{
+            scale: 1,
+            yoyo:true,
+            duration:.3,
+            ease: "power1.out",
+        })
+    
+    })
+}
+
+mycursor()
